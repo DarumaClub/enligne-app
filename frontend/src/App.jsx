746ef3911c8c7918cd272c9be43432fc9922d52e
“Brahim” <brahim.benallel@probance.com>
2025-05-28 21:06:02 +0900

    🔁 Connexion au VPS Express API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [ping, setPing] = useState("...");
 
   useEffect(() => {
-    fetch("http://localhost:3000/ping")
+    fetch("http://194.163.45.122:3000/ping")
       .then((res) => res.json())
       .then((data) => setPing(data.message))
       .catch((err) => setPing("Erreur API ❌"));
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Hello Brahim 👋</h1>
-      <p className="mt-4 text-lg text-gray-700">Réponse API : {ping}</p>
+      <p className="mt-4 text-lg text-green-700">Réponse API : {ping}</p>
     </div>
   );
 }
